70f946e8a5c7b21ba24417543ab11482191f645f
thiago <thiagoprocaci@gmail.com>
2018-05-29 22:20:11 -0300

    java8-junit5-archetype

---
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,14 @@
+package ${package};
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    void myFirstTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
